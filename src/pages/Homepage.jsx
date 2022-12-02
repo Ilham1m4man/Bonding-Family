e5e18371884fd1737785fd0f4137b8ef5376a12b
Dzikri Ardafi <dzkri.ardfi@gmail.com>
2022-12-02 23:13:47 +0700

    first commit

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,9 +1,11 @@
 import React from "react";
+import Sidebar from "../components/HomepageComponent/Sidebar";
 
 const HomePage = () => {
   return (
     <>
       <h1>this is my home page</h1>
+      <Sidebar></Sidebar>
     </>
   );
 };
