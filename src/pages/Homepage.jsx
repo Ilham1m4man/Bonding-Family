8ba62a43b5b30506c22874529c7c096236e8d735
Dzikri Ardafi <dzkri.ardfi@gmail.com>
2022-12-02 22:53:50 +0700

    Initialize Awal

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 
-function HomePage () {
-    return (
-        <>
-            <h1>this is my home page</h1>
-        </>
-    )
-}
+const HomePage = () => {
+  return (
+    <>
+      <h1>this is my home page</h1>
+    </>
+  );
+};
 
-export default HomePage
+export default HomePage;
