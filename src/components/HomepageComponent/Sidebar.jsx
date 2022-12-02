e876ce6f9ca6e758c7bf4757a293677f562a7de5
Dzikri Ardafi <dzkri.ardfi@gmail.com>
2022-12-02 23:29:15 +0700

    Initialize awal

--- src/components/HomepageComponent/Sidebar.jsx
+++ src/components/HomepageComponent/Sidebar.jsx
@@ -1,9 +1,11 @@
 import React from "react";
-
+import LogoBonding from "../../../public/assets/Images/Logo-Bonding-Family.svg";
 const Sidebar = () => {
   return (
     <>
-      <div className="sidebar"></div>
+      <div className="sidebar">
+        <img src={LogoBonding} alt="logo bonding family"></img>
+      </div>
     </>
   );
 };
