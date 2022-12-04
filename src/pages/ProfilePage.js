cd4893c7586369806aae9ac669fb531df96a183b
Ilham1m4man <kucingdiki@gmail.com>
2022-12-04 23:38:12 +0700

    Add profile page js

--- src/pages/ProfilePage.js
+++ src/pages/ProfilePage.js
@@ -1,11 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
+import WelcomeToApp from "../components/HomepageComponent/Welcome";
+import Sidebar from "../components/HomepageComponent/Sidebar/Sidebar";
+import SidebarCopy from "../components/HomepageComponent/Sidebar/Sidebar copy";
 
 const ProfilePage = () => {
+  const [isSidebar, setSidebar] = useState(false);
+  const openSidebar = () => {
+    setSidebar(true);
+    document.getElementById("sidebar").style.display = "block";
+  };
+
+  const closeSidebar = () => {
+    setSidebar(false);
+    document.getElementById("sidebar").style.display = "none";
+  };
   return (
     <>
-      <h1>Profile Page</h1>
+      <div style={{ display: "flex" }}>
+        {isSidebar === false ? (
+          isSidebar === false
+        ) : (
+          <div>
+            <Sidebar closeSidebar={closeSidebar}></Sidebar>
+          </div>
+        )}
+
+        {isSidebar === true ? (
+          isSidebar === true
+        ) : (
+          <div>
+            <SidebarCopy></SidebarCopy>
+          </div>
+        )}
+
+        <div className="profileContent" style={{ margin: "2em 3em", width: "100%" }}>
+          <WelcomeToApp openSidebar={openSidebar}></WelcomeToApp>
+
+        </div>
+      </div>
     </>
-  )
-}
+  );
+};
 
 export default ProfilePage;
