f11efbe2464f72c65aaf8be14fab465d4c28823b
Ilham1m4man <kucingdiki@gmail.com>
2022-12-07 14:05:27 +0700

    Add jadwal main content file

---
+++ src/components/JadwalPageComponent/JadwalMainContent.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "../../styles/jadwal-main-comp-style.css";
+
+const JadwalMainContent = () => {
+  return (
+    <>
+      <div id="jadwal-container" className="mainContent">
+        <h1>Jadwal</h1>
+      </div>
+    </>
+  );
+};
+
+export default JadwalMainContent;
