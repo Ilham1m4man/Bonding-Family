929728c58e0fb54df28b7adc7cbaa38a8d40bc49
Ilham1m4man <kucingdiki@gmail.com>
2022-12-07 14:06:35 +0700

    Add profile icon file

---
+++ src/components/SectionTop/ProfileIcon.js
@@ -0,0 +1,18 @@
+/* eslint-disable jsx-a11y/img-redundant-alt */
+import React from "react";
+import { Link } from "react-router-dom";
+import ProfileAvatar from "../../assets/icons/Avatar-profile.svg";
+import path from "../../utils/path-name";
+import "../../styles/navbar-style.css";
+
+const ProfileIcon = () => {
+  return (
+    <>
+      <Link to={path.profile} className="profile-icon">
+        <img className="profileOnMobileWidth" src={ProfileAvatar} alt="profile picture"/>
+      </Link>
+    </>
+  );
+};
+
+export default ProfileIcon;
