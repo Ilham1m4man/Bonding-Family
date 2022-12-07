8b06d01a17bf1c100b0880036aec22aa9932c17f
Ilham1m4man <kucingdiki@gmail.com>
2022-12-07 12:32:43 +0700

    Convert button to Link

--- src/components/SectionTop/ProfileIcon.js
+++ src/components/SectionTop/ProfileIcon.js
@@ -1,14 +1,16 @@
 /* eslint-disable jsx-a11y/img-redundant-alt */
 import React from "react";
+import { Link } from "react-router-dom";
 import ProfileAvatar from "../../assets/icons/Avatar-profile.svg";
+import path from "../../utils/path-name";
 import "../../styles/navbar-style.css";
 
 const ProfileIcon = () => {
   return (
     <>
-      <button className="profile-icon">
+      <Link to={path.profile} className="profile-icon">
         <img className="profileOnMobileWidth" src={ProfileAvatar} alt="profile picture"/>
-      </button>
+      </Link>
     </>
   );
 };
