413967db5cc7ae66ba14bcedde6af08c74e9b4dd
Ilham1m4man <kucingdiki@gmail.com>
2022-12-07 12:38:10 +0700

    Add template creator file

--- src/views/templates/template-creator.js
+++ src/views/templates/template-creator.js
@@ -1,9 +1,37 @@
 import React, { useState } from "react";
+import { useLocation } from "react-router-dom";
+import JadwalMainContent from "../../components/JadwalPageComponent/JadwalMainContent";
+import ProfileMainContent from "../../components/ProfilePageComponent/MainComponent/ProfileMainContent";
 import NavbarMobile from "../../components/SectionTop/NavbarMobile";
 import Sidebar from "../../components/Sidebar/Sidebar";
 import SidebarCopy from "../../components/Sidebar/Sidebar copy";
+import path from "../../utils/path-name";
 
 const CreateNavTemplate = () => {
+  const location = useLocation();
+  let pageSaatIni;
+  
+  if (location.pathname === path.home) {
+    /* pageSaatIni = <HomeMainContent key={0}></HomeMainContent>; */
+  } else if (location.pathname === path.survey) {
+    // TO DO: Bikin isian/content survey page
+    /* pageSaatIni = <SurveyMainContent key={1}></SurveyMainContent>; */
+    pageSaatIni = <h1>Survey Mingguan</h1>;
+  } else if (location.pathname === path.jadwal) {
+    pageSaatIni = <JadwalMainContent key={2}></JadwalMainContent>;
+  } else if (location.pathname === path.interaksi) {
+    // TO DO: Bikin isian/content interaksi page
+    /* pageSaatIni = <InteraksiMainContent key={3}></InteraksiMainContent>; */
+    pageSaatIni = <h1>Interaksi</h1>;
+  } else if (location.pathname === path.tipsNTrick) {
+    // TO DO: Bikin isian/content tips&trick page
+    /* pageSaatIni = <TipsNTrickMainContent key={4}></TipsNTrickMainContent>; */
+    pageSaatIni = <h1>Tips & Trick</h1>;
+  } else if (location.pathname === path.profile) {
+    // TO DO: Bikin isian/content survey page
+    pageSaatIni = <ProfileMainContent key={5}></ProfileMainContent>;
+  }
+
   const [isSidebar, setSidebar] = useState(false);
   const openSidebar = () => {
     setSidebar(true);
@@ -16,7 +44,7 @@ const CreateNavTemplate = () => {
   };
   return (
     <>
-      <div id="profile-page">
+      <div id={pageSaatIni.key === '5' ? "profile-page" : "sidebar-navbar-page"}>
         {isSidebar === false ? (
           isSidebar === false
         ) : (
@@ -32,9 +60,9 @@ const CreateNavTemplate = () => {
             <SidebarCopy></SidebarCopy>
           </div>
         )}
-
-        <div className="contentRightSide">
+        <div className={pageSaatIni.key === '5' ? "profileContentRightSide" : "contentRightSide"}>
           <NavbarMobile openSidebar={openSidebar}></NavbarMobile>
+          {pageSaatIni}
         </div>
       </div>
     </>
