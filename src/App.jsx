c3aec80be022c5fb34fbd0290e53086c609cff4c
Ilham1m4man <kucingdiki@gmail.com>
2022-12-11 05:14:00 +0700

    Add tambah jadwal route

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,18 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 import ProfilePage from "./views/pages/ProfilePage";
 import JadwalPage from "./views/pages/JadwalPage";
+import TambahJadwalPage from "./views/pages/TambahJadwalPage";
 import "./main.css";
+import path from "./utils/path-name";
 
 const BondingFams = () => {
   return (
     <>
       <main>
         <Routes>
-          <Route path="/profile" element={<ProfilePage />} />
-          <Route path="/jadwal" element={<JadwalPage />} />
+          <Route path={path.profile} element={<ProfilePage />} />
+          <Route path={path.jadwal} element={<JadwalPage />} />
+          <Route path={path.tambahJadwal} element={<TambahJadwalPage />} />
         </Routes>
       </main>
     </>
