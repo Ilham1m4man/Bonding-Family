bc7878ccc284b45a47531482bd729f8898a8a3d5
Ilham1m4man <kucingdiki@gmail.com>
2022-12-11 05:14:00 +0700

    Add tambah jadwal route

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React, { useEffect } from "react";
-import { Route, Routes, useLocation, useNavigate } from "react-router-dom";
+import { Route, Routes, useNavigate } from "react-router-dom";
 import ProfilePage from "./views/pages/ProfilePage";
 import JadwalPage from "./views/pages/JadwalPage";
 import HomePage from "./views/pages/Homepage";
 import InteraksiPage from "./views/pages/Interaksi";
 import LoginPage from "./views/pages/Loginpage";
 import SignUp from "./views/pages/SignUp";
+import TambahJadwalPage from "./views/pages/TambahJadwalPage";
 import "./main.css";
+import path from "./utils/path-name";
 
 const BondingFams = () => {
   const navigate = useNavigate();
@@ -27,6 +29,7 @@ const BondingFams = () => {
           <Route path="/jadwal" element={<JadwalPage />} />
           <Route path="/login" element={<LoginPage />} />
           <Route path="/sign-up" element={<SignUp />} />
+          <Route path={path.tambahJadwal} element={<TambahJadwalPage />} />
         </Routes>
       </main>
     </>
