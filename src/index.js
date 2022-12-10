84c8a1e859ebcd70d28138259e5359eaf441b1cb
Dzikri Ardafi <dzkri.ardfi@gmail.com>
2022-12-10 23:11:17 +0700

    first initilize integrate

--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
-import { createRoot } from 'react-dom/client';
- import {BrowserRouter} from 'react-router-dom'
- import BondingFams from './App';
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import BondingFams from "./App";
 
- 
-const root = createRoot(document.getElementById('root'));
+const root = createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter>
-    <BondingFams/>
+    <BondingFams />
   </BrowserRouter>
-)
+);
